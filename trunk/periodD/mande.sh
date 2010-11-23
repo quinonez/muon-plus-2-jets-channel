1a20b7fc60520d915a0bf796cf1eef43a55110d6
fnananf <fnananf@9b2c81d1-efdb-e9b1-d03d-0136e1d96659>
2010-11-23 00:34:52 +0000

    periodD erased

--- trunk/periodD/mande.sh
+++
@@ -1,5 +0,0 @@
-prun --bexec "make" --exec "corra %IN" --athenaTag=16.0.1 --outputs file.root --outDS user.quinonez.periodD.2010oct25.V2 \
---inDS group10.phys-susy.SUSYD3PD.data10_7TeV.periodD.physics_MuonswBeam.PhysCont.AOD.t0pro04_v01.V2.1_D2PDSUSY_Muon.000614.V2 \
---nFilesPerJob=1 --extFile=root
-
-
